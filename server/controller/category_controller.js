ff7c1076d71f85762419b17ae50526f86c8caf86
AdrianGalindez <adriangalindez@unicomfacauca.edu.co>
2026-03-19 22:20:30 -0500

    Update category_controller.js

--- server/controller/category_controller.js
+++ server/controller/category_controller.js
@@ -1,29 +1,29 @@
-var Categoriadb = require('../model/categoria');
+var Categorydb = require('../model/categoria');
 
 exports.create = (req,res)=>{
-    const categoria = new Categoriadb({
+    const Categorydb = new Categoriadb({
         nombre : req.body.nombre
     });
 
-    categoria.save()
+    Categorydb.save()
         .then(data => res.send(data))
         .catch(err => res.status(500).send(err))
 }
 
 exports.find = (req,res)=>{
-    Categoriadb.find()
+    Categorydb.find()
         .then(data => res.send(data))
         .catch(err => res.status(500).send(err))
 }
 
 exports.update = (req,res)=>{
-    Categoriadb.findByIdAndUpdate(req.params.id, req.body)
+    Categorydb.findByIdAndUpdate(req.params.id, req.body)
         .then(data => res.send(data))
         .catch(err => res.status(500).send(err))
 }
 
 exports.delete = (req,res)=>{
-    Categoriadb.findByIdAndDelete(req.params.id)
+    Categorydb.findByIdAndDelete(req.params.id)
         .then(data => res.send({ message : "Categoria eliminada"}))
         .catch(err => res.status(500).send(err))
 }
